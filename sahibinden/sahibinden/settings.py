446ee8c70f5729bff58a378dc198e6f8be5fc270
merveealpay <merveealpay@gmail.com>
2020-10-01 23:30:20 +0300

    django all-auth

--- sahibinden/sahibinden/settings.py
+++ sahibinden/sahibinden/settings.py
@@ -29,6 +29,16 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+AUTHENTICATION_BACKENDS = [
+
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,8 +47,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
     'advertise',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
 ]
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
