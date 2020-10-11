9ce8200fe907e41de926034ca98ee3f5221e9923
merveealpay <merveealpay@gmail.com>
2020-10-11 15:22:11 +0300

    static files and templates

--- sahibinden/sahibinden/settings.py
+++ sahibinden/sahibinden/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'sahibinden.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -146,6 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Custom User
 AUTH_USER_MODEL = "advertise.User"
